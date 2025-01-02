ad0c6f4182c18448a7fd971ad81557559900c41f
Darik A O’Neil <darik.oneil.software@gmail.com>
2025-01-02 10:01:11 -0500

    Update test_a_install.py

--- tests/a_test_installation/test_a_install.py
+++ tests/a_test_installation/test_a_install.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import os
 import subprocess
 import sys
@@ -7,8 +8,18 @@ import pytest
 import toml
 
 
+"""
+A super disgusting way to test the packages install on WINDOWS ONLY. 
+I wrote this when I first learned python, but it works so I kepe it around for the lols.
+"""
+def retrieve_details(path) -> tuple[str, str, list[str]]:
+    """
+    Get the package details
 
-def retrieve_details(path):
+    :param path: The path to the package's pyproject.toml
+    :returns: The name, version, and dependencies of the package
+    """
+    
     details = toml.load(path).get("project")
     name = details.get("name")
     version = details.get("version")
@@ -16,11 +27,24 @@ def retrieve_details(path):
     return name, version, dependencies
 
 
-def retrieve_project_directory(path):
+def retrieve_project_directory(path) -> Path:
+    """
+    Retrieves the project's directory
+
+    :param path: Child path to project directory
+    :returns: The path to the project's directory
+    """
     return Path(path).parent
 
 
-def retrieve_project_file():
+def retrieve_project_file() -> os.PathLike:
+    """
+    Retrieves the project file (pyproject.toml)
+
+    :returns: The project file
+    :raises FileNotFoundError: if not found
+    """
+    
     project_file = os.path.join(os.getcwd(), "pyproject.toml")
     if not os.path.exists(project_file):
         project_file = os.path.join(Path(os.getcwd()).parent, "pyproject.toml")
@@ -29,7 +53,12 @@ def retrieve_project_file():
     return project_file
 
 
-def collect_project():
+def collect_project() -> tuple[Path, os.PathLike, str, str, list[str]]:
+    """
+    Collects the project
+
+    :returns: The project details (directory, file, name, version, dependencies)
+    """
     project_file = retrieve_project_file()
     project_directory = retrieve_project_directory(project_file)
     package_name, package_version, package_dependencies = retrieve_details(project_file)

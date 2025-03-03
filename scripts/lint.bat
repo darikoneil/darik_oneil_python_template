40d10863687c9642a72f6e9e3d79375821303cb8
Darik A O’Neil <darik.oneil.software@gmail.com>
2025-03-03 14:55:29 -0500

    Update lint.bat

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -5,13 +5,13 @@ setlocal enabledelayedexpansion
 cd ..
 
 echo [0;33m "Formatting (RUFF)..." [0m
-ruff format ./!%PROJECT_NAME%! ./tests
+ruff format ./{{ project_name }} ./tests
 
 echo [0;33m "Linting (RUFF)..." [0m
-ruff check ./!%PROJECT_NAME%! ./tests -o .ruff.json --output-format json --fix --no-cache
+ruff check ./{{ project_name }} ./tests -o .ruff.json --output-format json --fix --no-cache
 
 :: too opinionated for linting tests
 echo [0;33m "Linting (FLAKE8 PLUGINS)..." [0m
-flake8 ./!%PROJECT_NAME%!
+flake8 ./{{ project_name }}
 
 echo [0;33m "Finished Formatting & Linting
